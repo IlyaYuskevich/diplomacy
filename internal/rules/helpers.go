65f0c00fa5bba0b27a7ab219a84344ce9c02bf5a
Ilya Iuskevich <ilya.iuskevich@alemira.com>
2023-04-16 21:39:10 +0200

    add movable fleet, adjacent provinces debugging

--- internal/rules/helpers.go
+++ internal/rules/helpers.go
@@ -1,6 +1,6 @@
 package rules
 
-func GetPossibleMoves(Origin string) map[string]string {
+func GetPossibleArmyMoves(Origin string) map[string]string {
 	moves := ArmyBorders[Origin]
 
 	possibleMoves := map[string]string{}
@@ -10,3 +10,15 @@ func GetPossibleMoves(Origin string) map[string]string {
 	}
 	return possibleMoves
 }
+
+func GetPossibleFleetMoves(Origin string) map[string]string {
+	moves := FleetBorders[Origin]
+
+	possibleMoves := map[string]string{}
+
+	for _, v := range moves {
+		possibleMoves[v] = Provinces[v].Name
+	}
+	return possibleMoves
+}
+
