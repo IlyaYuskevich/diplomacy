1c0e012b5f5ed91edfa87bec5bf1db2261da563e
Ilya Iuskevich <ilya.iuskevich@alemira.com>
2023-06-18 21:26:19 +0200

    make json fields camelCase

--- api/handlers/moves_test.go
+++ api/handlers/moves_test.go
@@ -16,10 +16,10 @@ import (
 )
 
 var (
-	sampleMove1Json        = `{"id":"d42830mv-a75c-40c5-ade3-56a38db0fd01","created_at":"2023-02-18T14:45:13.69505Z","type":"support","origin":"Par","from":"Par","to":"Bur","phase":"S","year":1903,"unit_type":"A","status":"submitted","game_id":"d42830gg-a75c-40c5-ade3-56a38db0fd01","player_game_id":"d42830pg-a75c-40c5-ade3-56a38db0fd01","player_game":{"id":"d42830pg-a75c-40c5-ade3-56a38db0fd01","country":"england","color":"white"}}`
-	sampleMoveJsonPayload1 = `{"type":"move","origin":"Ber","to":"Mun","phase":"S","year":1903,"unit_type":"A","player_game_id":"d42830pg-a75c-40c5-ade3-56a38db0fd02","game_id":"d42830gg-a75c-40c5-ade3-56a38db0fd01"}`
+	sampleMove1Json        = `{"id":"d42830mv-a75c-40c5-ade3-56a38db0fd01","createdAt":"2023-02-18T14:45:13.69505Z","type":"support","origin":"Par","from":"Par","to":"Bur","phase":"S","year":1903,"unitType":"A","status":"submitted","gameId":"d42830gg-a75c-40c5-ade3-56a38db0fd01","playerGameId":"d42830pg-a75c-40c5-ade3-56a38db0fd01","playerGame":{"id":"d42830pg-a75c-40c5-ade3-56a38db0fd01","country":"england","color":"white"}}`
+	sampleMoveJsonPayload1 = `{"type":"move","origin":"Ber","to":"Mun","phase":"S","year":1903,"unitType":"A","playerGameId":"d42830pg-a75c-40c5-ade3-56a38db0fd02","gameId":"d42830gg-a75c-40c5-ade3-56a38db0fd01"}`
 	sampleMoveJsonPayload2 = `{"status":"undone"}`
-	sampleMove1Patched     = `{"id":"d42830mv-a75c-40c5-ade3-56a38db0fd01","created_at":"2023-02-18T14:45:13.69505Z","type":"support","origin":"Par","from":"Par","to":"Bur","phase":"S","year":1903,"unit_type":"A","status":"undone","game_id":"d42830gg-a75c-40c5-ade3-56a38db0fd01","player_game_id":"d42830pg-a75c-40c5-ade3-56a38db0fd01","player_game":{"id":"d42830pg-a75c-40c5-ade3-56a38db0fd01","country":"england","color":"white"}}`
+	sampleMove1Patched     = `{"id":"d42830mv-a75c-40c5-ade3-56a38db0fd01","createdAt":"2023-02-18T14:45:13.69505Z","type":"support","origin":"Par","from":"Par","to":"Bur","phase":"S","year":1903,"unitType":"A","status":"undone","gameId":"d42830gg-a75c-40c5-ade3-56a38db0fd01","playerGameId":"d42830pg-a75c-40c5-ade3-56a38db0fd01","playerGame":{"id":"d42830pg-a75c-40c5-ade3-56a38db0fd01","country":"england","color":"white"}}`
 )
 
 var sampleMoves = []types.Move{
