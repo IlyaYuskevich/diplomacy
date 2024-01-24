c6b0866784b58faa59dcedbf16ff2f9ec6e5976d
Ilya Yuskevich <ilya.yuskevich@gmail.com>
2024-01-24 22:01:23 +0100

    allow empty values

--- src/lib/environment.ts
+++ src/lib/environment.ts
@@ -1,6 +1,6 @@
 import { load } from "dotenv/mod.ts";
 
-await load({ export: true });
+await load({ export: true, allowEmptyValues: true });
 
 export const SUPABASE_URL = Deno.env.get("SUPABASE_URL") || "";
 export const SUPABASE_KEY = Deno.env.get("SUPABASE_KEY") || "";
